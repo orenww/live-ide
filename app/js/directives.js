1b5e45fbf802e64a4e007b9f3519a8f438a9a6ce
orenww <orenw@Orenw-PC.bevents.local>
2013-08-12 10:54:32 +0300

    work with option theme, mode etc

--- app/js/directives.js
+++ app/js/directives.js
@@ -12,26 +12,83 @@ angular.module('myApp.directives', []).
   .directive('uiAce', function(){
   	return {
   		restrict: 'EA',
+      require: '?ngModel',
   		template: '<div id="editor"></div>',
   		replace: true,
 
   		link: function(scope, element, attrs) {
+
+        
+
   			// trigger extension
   			ace.require("ace/ext/language_tools");
   			var editor = ace.edit("editor");
-  			editor.session.setMode("ace/mode/sql");
-        editor.setTheme("ace/theme/tomorrow");
+  			//editor.session.setMode("ace/mode/sql");
+        //editor.setTheme("ace/theme/tomorrow");
+
   			// enable autocompletion and snippets
   			editor.setOptions({
   			    enableBasicAutocompletion: true,
   			    enableSnippets: true
   			});
 
+        var options, opts, acee, session, onChange;
+        opts = angular.extend({}, options, scope.$eval(attrs.uiAceOptions));
+
+        acee = editor;
+        session = acee.getSession();
+
+        onChange = function (callback) {
+          return function (e) {
+            var newValue = session.getValue();
+            if (newValue !== scope.$eval(attrs.value) && !scope.$$phase) {
+              if (angular.isDefined(ngModel)) {
+                scope.$apply(function () {
+                  ngModel.$setViewValue(newValue);
+                });
+              }
+
+              /**
+               * Call the user onChange function.
+               */
+              if (angular.isDefined(callback)) {
+                scope.$apply(function () {
+                  if (angular.isFunction(callback)) {
+                    callback(e, acee);
+                  }
+                  else {
+                    throw new Error('ui-ace use a function as callback.');
+                  }
+                });
+              }
+            }
+          };
+        };
+
+        // Boolean options
+        if (angular.isDefined(opts.showGutter)) {
+          acee.renderer.setShowGutter(opts.showGutter);
+        }
+        if (angular.isDefined(opts.useWrapMode)) {
+          session.setUseWrapMode(opts.useWrapMode);
+        }        
+        // Basic options
+        if (angular.isString(opts.theme)) {
+          acee.setTheme("ace/theme/" + opts.theme);
+        }
+        if (angular.isString(opts.mode)) {
+          session.setMode("ace/mode/" + opts.mode);
+        }
+
+
         //set content
-        var lines = editor.session.doc.$lines;
-        lines[0] = "set attribute 'content into line 1 - " + attrs.content;
-        lines[1] = "hard code line 2";
-        lines[2] = "hard code line 3";
+        acee.setValue(attrs.content);
+
+
+        // var lines = editor.session.doc.$lines;
+        // lines[0] = "set attribute 'content into line 1 - " + attrs.content;
+        // lines[1] = "hard code line 2";
+        // lines[2] = "hard code line 3";
   		}
   	}
   });
