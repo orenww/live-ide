dfbe9788085b9aa29c6546ad24fceaf4fbfc6ffd
orenww <orenw@Orenw-PC.bevents.local>
2013-08-12 18:04:09 +0300

    add the tree from scratch, still not working

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,7 +2,7 @@
 
 /* Controllers */
 
-angular.module('myApp.controllers', []).
+angular.module('myApp.controllers', ['angularTreeview']).
   controller('MyCtrl1', [function($scope) {
   	var editor = ace.edit("editor");
   }])
@@ -19,21 +19,92 @@ angular.module('myApp.controllers', []).
     $scope.$watch('content',function(){
       console.log("from the controller - " + $scope.content);
     });
+
+
+ $scope.treedata = 
+    [
+        { "label" : "User", "id" : "role1", "children" : [
+            { "label" : "subUser1", "id" : "role11", "children" : [] },
+            { "label" : "subUser2", "id" : "role12", "children" : [
+                { "label" : "subUser2-1", "id" : "role121", "children" : [
+                    { "label" : "subUser2-1-1", "id" : "role1211", "children" : [] },
+                    { "label" : "subUser2-1-2", "id" : "role1212", "children" : [] }
+                ]}
+            ]}
+        ]},
+        { "label" : "Admin", "id" : "role2", "children" : [] },
+        { "label" : "Guest", "id" : "role3", "children" : [] }
+    ];   
+    //test tree model 1
+    $scope.roleList1 = [
+        { "roleName" : "User", "roleId" : "role1", "children" : [
+          { "roleName" : "subUser1", "roleId" : "role11", "children" : [] },
+          { "roleName" : "subUser2", "roleId" : "role12", "children" : [
+            { "roleName" : "subUser2-1", "roleId" : "role121", "children" : [
+              { "roleName" : "subUser2-1-1", "roleId" : "role1211", "children" : [] },
+              { "roleName" : "subUser2-1-2", "roleId" : "role1212", "children" : [] }
+            ]}
+          ]}
+        ]},
+
+        { "roleName" : "Admin", "roleId" : "role2", "children" : [] },
+
+        { "roleName" : "Guest", "roleId" : "role3", "children" : [] }
+      ];
+
+    //test tree model 2
+    $scope.roleList2 = [
+        { "roleName" : "User", "roleId" : "role1", "children" : [
+          { "roleName" : "subUser1", "roleId" : "role11", "collapsed" : true, "children" : [] },
+          { "roleName" : "subUser2", "roleId" : "role12", "collapsed" : true, "children" : [
+            { "roleName" : "subUser2-1", "roleId" : "role121", "children" : [
+              { "roleName" : "subUser2-1-1", "roleId" : "role1211", "children" : [] },
+              { "roleName" : "subUser2-1-2", "roleId" : "role1212", "children" : [] }
+            ]}
+          ]}
+        ]},
+
+        { "roleName" : "Admin", "roleId" : "role2", "children" : [
+          { "roleName" : "subAdmin1", "roleId" : "role11", "collapsed" : true, "children" : [] },
+          { "roleName" : "subAdmin2", "roleId" : "role12", "children" : [
+            { "roleName" : "subAdmin2-1", "roleId" : "role121", "children" : [
+              { "roleName" : "subAdmin2-1-1", "roleId" : "role1211", "children" : [] },
+              { "roleName" : "subAdmin2-1-2", "roleId" : "role1212", "children" : [] }
+            ]}
+          ]}
+        ]},
+
+        { "roleName" : "Guest", "roleId" : "role3", "children" : [
+          { "roleName" : "subGuest1", "roleId" : "role11", "children" : [] },
+          { "roleName" : "subGuest2", "roleId" : "role12", "collapsed" : true, "children" : [
+            { "roleName" : "subGuest2-1", "roleId" : "role121", "children" : [
+              { "roleName" : "subGuest2-1-1", "roleId" : "role1211", "children" : [] },
+              { "roleName" : "subGuest2-1-2", "roleId" : "role1212", "children" : [] }
+            ]}
+          ]}
+        ]}
+      ];
+
+      
+      
+    //roleList1 to treeview
+    $scope.roleList = $scope.roleList1;
+    
   }).controller('TreeCtrl', function($scope){
-    // $scope.treedata = 
-    // [
-    //     { "label" : "User", "id" : "role1", "children" : [
-    //         { "label" : "subUser1", "id" : "role11", "children" : [] },
-    //         { "label" : "subUser2", "id" : "role12", "children" : [
-    //             { "label" : "subUser2-1", "id" : "role121", "children" : [
-    //                 { "label" : "subUser2-1-1", "id" : "role1211", "children" : [] },
-    //                 { "label" : "subUser2-1-2", "id" : "role1212", "children" : [] }
-    //             ]}
-    //         ]}
-    //     ]},
-    //     { "label" : "Admin", "id" : "role2", "children" : [] },
-    //     { "label" : "Guest", "id" : "role3", "children" : [] }
-    // ];   
+    $scope.treedata = 
+    [
+        { "label" : "User", "id" : "role1", "children" : [
+            { "label" : "subUser1", "id" : "role11", "children" : [] },
+            { "label" : "subUser2", "id" : "role12", "children" : [
+                { "label" : "subUser2-1", "id" : "role121", "children" : [
+                    { "label" : "subUser2-1-1", "id" : "role1211", "children" : [] },
+                    { "label" : "subUser2-1-2", "id" : "role1212", "children" : [] }
+                ]}
+            ]}
+        ]},
+        { "label" : "Admin", "id" : "role2", "children" : [] },
+        { "label" : "Guest", "id" : "role3", "children" : [] }
+    ];   
     //test tree model 1
     $scope.roleList1 = [
         { "roleName" : "User", "roleId" : "role1", "children" : [
