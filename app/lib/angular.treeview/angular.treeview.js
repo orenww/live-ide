f595faa25ffe62e4017c396e0985b6489eb37ed1
Oren Farhi <farhioren@gmail.com>
2013-08-20 16:00:17 +0300

    added bootstrap.less, changed html tags, added flexbox styles

--- app/lib/angular.treeview/angular.treeview.js
+++ app/lib/angular.treeview/angular.treeview.js
@@ -53,14 +53,14 @@
 
 var template = 
 '<ul class="unstyled">' + 
-	'<li data-ng-repeat="node in ' + treeModel + '" class="tree-node-wrapper">' + 
+	'<li data-ng-repeat="node in ' + treeModel + '">' + 
 		'<div class="tree-node" data-ng-class="node.selected" data-ng-click="selectNodeLabel(node)">' +
 			'<i class="collapsed" data-ng-show="node.' + nodeChildren + '.length && node.collapsed" data-ng-click="selectNodeHead(node)"></i>' + 
 			'<i class="expanded" data-ng-show="node.' + nodeChildren + '.length && !node.collapsed" data-ng-click="selectNodeHead(node)"></i>' + 
 			'<i class="normal" data-ng-hide="node.' + nodeChildren + '.length"></i> ' + 
 			'{{node.' + nodeLabel + '}}' +
 		'</div>' + 
-		'<div class="tree-node-childs" data-ng-hide="node.collapsed" data-tree-model="node.' + nodeChildren + '" data-node-id=' + nodeId + ' data-node-label=' + nodeLabel + ' data-node-children=' + nodeChildren + '></div>' + 
+		'<div data-ng-hide="node.collapsed" data-tree-model="node.' + nodeChildren + '" data-node-id=' + nodeId + ' data-node-label=' + nodeLabel + ' data-node-children=' + nodeChildren + '></div>' + 
 	'</li>' + 
 '</ul>'; 
 
