1825803e6f7aaeafb35f62b3873e1ac2d73ac1f4
orenww <orenw@Orenw-PC.bevents.local>
2013-10-13 15:19:39 +0300

    Add snippet filter

--- app/js/controllers/SnippetsCtrl.js
+++ app/js/controllers/SnippetsCtrl.js
@@ -1,22 +1,76 @@
 vStudio.controllers.controller('SnippetsCtrl', function ($scope, AceSnippetsExtensionService, AutoCompleteService) {
   
-  $scope.snippetsObject = {};
+  $scope.search = "";
 
-  $scope.getSnippets = function() {         
+  $scope.array = [];
 
-    var serverSnippetsObj = AutoCompleteService.getSnippets();
-    if(serverSnippetsObj){
-      $scope.snippetsObject = serverSnippetsObj;      
-    }
+  $scope.isGotEditorSnippets = false;
+
+  $scope.getSnippets = function() {
+
+    var snippetsObject = {};
+
+    var serverSnippetsObj = AutoCompleteService.getSnippets();    
     
     var editorsSnippetsArray = AceSnippetsExtensionService.getEditorSnippets();
 
-    if($scope.snippetsObject){
-      angular.forEach(editorsSnippetsArray, function(value, key){
-        $scope.snippetsObject[key] = value;
-      });          
+    
+    if(editorsSnippetsArray != null && !$scope.isGotEditorSnippets){
+      $scope.array = $scope.array.concat(serverSnippetsObj,editorsSnippetsArray);
+      $scope.isGotEditorSnippets = true;
     }
 
-    return $scope.snippetsObject;
-  }; 
+    return $scope.array;  
+  };
+
+  // $scope.search = "";
+
+  // $scope.array = [];
+
+  // $scope.isGotEditorSnippets = false;
+  // $scope.isGotServerSnippets = false;
+
+  // $scope.getSnippets = function() {
+
+  //   var snippetsObject = {};
+
+  //   var serverSnippetsObj = AutoCompleteService.getSnippets();
+  //   //return serverSnippetsObj;
+
+  //   // if(serverSnippetsObj){
+  //   //   snippetsObject = serverSnippetsObj;      
+  //   // }
+    
+  //   $scope.snippetsArr = AceSnippetsExtensionService.getEditorSnippets();
+
+  //   // if(snippetsObject){
+  //   //   angular.forEach(editorsSnippetsArray, function(value, key){
+  //   //     snippetsObject[key] = value;
+  //   //   });          
+  //   // }
+
+
+
+  //   if($scope.snippetsArr != null && !$scope.isGotEditorSnippets){
+  //     $scope.array = $scope.array.concat(serverSnippetsObj,$scope.snippetsArr);
+  //     $scope.isGotEditorSnippets = true;
+  //   }
+
+  //   return $scope.array;  
+  // };
+
+  // $scope.snippetsArr = AceSnippetsExtensionService.getEditorSnippets();
+  // $scope.$watch( 'snippetsArr', function (snippets, oldSnippets) {
+  //   for (var i = 0; i < snippets.length; i++) {
+  //     $scope.array.push(snippets[i]);
+  //   };
+  // }, true);
+
+  // $scope.serverSnippets = AutoCompleteService.getSnippets();
+  // $scope.$watch( 'serverSnippets', function (snippets, oldSnippets) {
+  //   for (var i = 0; i < snippets.length; i++) {
+  //     $scope.array.push(snippets[i]);
+  //   };
+  // }, true)
+ 
 });
--- app/js/services/Constants.js
+++ app/js/services/Constants.js
@@ -7,7 +7,7 @@ vStudio.services.service('Constants', function() {
 	this.APP_URL = 'mock/app.descriptor.json';
 	//this.APP_URL = 'http://localhost:8080/AppDescriptorServlet?appId=app-1'; 
 
-	this.SCHEMA_URL = 'mock/intellisense.rules.server.json';
-	//this.SCHEMA_URL = 'mock/intellisense.rules.json';
+	//this.SCHEMA_URL = 'mock/intellisense.rules.server.json';
+	this.SCHEMA_URL = 'mock/intellisense.rules.json';
 	//this.SCHEMA_URL = 'http://localhost:8080/DbSchemeServlet';
 });
