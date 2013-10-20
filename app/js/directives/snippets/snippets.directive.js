f9f3797a9a499dcd130a5cb48c6e6d06f037d798
orenww <orenw@Orenw-PC.bevents.local>
2013-10-20 11:21:54 +0300

    snippet edit

--- app/js/directives/snippets/snippets.directive.js
+++ app/js/directives/snippets/snippets.directive.js
@@ -3,7 +3,7 @@
 
 	var app = angular.module( 'snippets', [] );
 	
-	app.directive('snippet', function(){
+	app.directive('snippet', function(AceSnippetsExtensionService){
 	   
 	    return {
 	        restrict: 'E',
@@ -15,7 +15,57 @@
 			templateUrl:'js/directives/snippets/snippetTemplate.html',
 	   
 	        link: function ( scope, element, attrs ) {	       		
-	       	}	     
+	       	},
+
+	       	controller: function($scope) {
+	       		$scope.data.orgContent = $scope.data.content;
+
+	       		var content = $scope.data.content;
+	       		var contentArray = content.split("\n\t");
+	       		$scope.data.content = contentArray[1];
+	       		
+
+	       		if($scope.data.description != undefined){
+	       			$scope.isDescription = true;	       			
+	       		}else{
+	       			$scope.isDescription = false;	       			
+	       		}
+
+	       		$scope.mode = "read";
+	       		$scope.isReadMode = true;
+	       		$scope.changeMode = function(){
+                	if($scope.isReadMode){
+            			$scope.isReadMode = false;
+						$scope.mode = "edit";            			
+                	}else{
+						$scope.isReadMode = true;
+						$scope.mode = "read";
+                	}
+  				}
+
+	       		$scope.snippetExpandClass = "";
+	       		$scope.iconExpandClass = "";
+
+                $scope.expandCollapseSnippet = function(){
+                	if($scope.iconExpandClass == ""){
+						$scope.iconExpandClass = "icon-rotate-90";
+						$scope.snippetExpandClass = "snippet-expand";
+                	}else{
+                		$scope.iconExpandClass = "";
+                		$scope.snippetExpandClass = "";
+                	}
+  				}
+
+  				$scope.save = function(){  					
+  					AceSnippetsExtensionService.editSnippet($scope.data.name,$scope.data.description,$scope.data.content);
+  				}
+
+  				$scope.delete = function(){  					
+  					AceSnippetsExtensionService.deleteSnippet($scope.data.name);
+  				}
+
+	       		return $scope;
+	       	}     
 	    };	    
 	});
 
